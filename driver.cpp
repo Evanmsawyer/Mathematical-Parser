0444a571f4a86de0e61dc6dd39273dc8babedb24
a-carney <77558389+a-carney@users.noreply.github.com>
2023-11-21 15:13:00 -0500

    Update driver.cpp
    renames inputParser to getIndexOfMathSymbols and various updates for proof of concept

--- driver.cpp
+++ driver.cpp
@@ -3,19 +3,7 @@
 
 
 
-int exeucte() {
-    // create a REPL for a calculator
 
-    // 1. read input from user
-    std::string input;
-    std::cout << "Enter an expression: ";
-
-    // 2. parse the input
-
-    // 3. evaluate the input
-    // 4. print the result
-    return 0;
-}
 
 /**
  * @brief check if a char is a mathematic symbol
@@ -28,17 +16,48 @@ bool isSymbol(char c) {
     return false;
 }
 
-int inputParser(std::string input) {
+/**
+ * @brief takes user input and returns indexes of mathematic symbols
+*/
+int getIndexOfMathSymbols(std::string input) {
     std::string result;
-    for (char c : input) {
+    for (std::string::size_type i = 0; i < input.size(); i++) {
+        char c = input[i];
         // look for char that is + - * / ( )
         if (isSymbol(c)) {
-            
+            // add index number to result string
+            result += std::to_string(i);
         }
 
     }
-    return 0;
+    // convert string to int and return
+    int resultInt = std::stoi(result);
+    return resultInt;
+}
 
+int parseInput(std::string input, int symbolIndexes) {
+    //TODO: break up String into tokens based on symbolIndexes
+    
+ }
+    
+
+    return -1;
+
+int exeucte() {
+    // create a REPL for a calculator
+
+    // 0. read input from user
+    std::string input;
+    std::cout << "Enter an expression: ";
+
+    // 1. parse the input
+    std::cin >> input;
+    int index = getIndexOfMathSymbols(input);
+
+    printf("index: %d\n", index);
+    // 2. evaluate the input
+    // 3. print the result
+    return -1;
 }
 
 int main() {
