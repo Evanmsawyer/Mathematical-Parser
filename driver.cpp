59d67f99f19f0728b5f538904c94dcf09f4406b7
a-carney <77558389+a-carney@users.noreply.github.com>
2023-11-27 16:50:39 -0500

    adds tokenization and use of vectors
    can test this by inputing a math expression and seeing it broken up into list.
    
    e.g.
    INPUT -> 2 + 2
    RESPONSE : 2, +, 2
    
    INPUT 22+22
    RESPONSE: 22, +, 22

--- driver.cpp
+++ driver.cpp
@@ -1,9 +1,12 @@
 #include <iostream>
 #include <string>
+#include <vector>
+#include <cctype>
+#include <cstdlib>
 
 
 
-enum {
+enum TokenType {
     ADD = 0, 
     SUB = 1, 
     MUL = 2, 
@@ -14,61 +17,93 @@ enum {
     END = 7 
 };
 
+enum PEMDAS {
+    PARENTHESES = 3,
+    EXPONENT = 2,
+    MULTIPLY_DIVIDE = 1,
+    ADD_SUBTRACT = 0,
+};
+
+struct Token {
+    TokenType type;
+    double value;
+};
+
 
 
 /**
  * @brief check if a char is a mathematic symbol
- * @returns true if c is a symbol, false otherwise
+ * @returns 
  */
-int categorizeCharForMath(char c) {
-    if (c == '+') {
-        return ADD;
-    } else if (c == '-') {
-        return SUB;
-    } else if (c == '*') {
-        return MUL;
-    } else if (c == '/') {
-        return DIV;
-    } else if (c == '(') {
-        return L_PAREN;
-    } else if (c == ')') {
-        return R_PAREN;
-    } else if (isdigit(c)) {
-        return NUM;
-    } else {
-        return END;
-    }
-    return -1;
+TokenType categorizeCharForMath(char c) {
+    switch (c) {
+        case '+': return ADD;
+        case '-': return SUB;
+        case '*': return MUL;
+        case '/': return DIV;
+        case '(': return L_PAREN;
+        case ')': return R_PAREN;
+        default: return isdigit(c) ? NUM : END;
+    } 
 }
 
 /**
- * @brief takes user input and returns indexes of mathematic symbols
+ * @brief returns order of operations for a given symbol 
+ * @returns order of operations 
 */
-std::string parseInput(std::string input) {
-    std::string result;
-    for (std::string::size_type i = 0; i < input.size(); i++) {
-        char c = input[i];
-        int charType = categorizeCharForMath(c);     
-        result += std::to_string(charType); 
-        
+int getOrderOfOperations(TokenType type) {
+    switch (type) {
+        case ADD: case SUB: return ADD_SUBTRACT;
+        case MUL: case DIV: return MULTIPLY_DIVIDE;
+        case L_PAREN: case R_PAREN: return PARENTHESES;
+        default: return -1;
     }
-    return result;
 }
 
-    
+/**
+ * @brief creates tokenized vector of input
+*/
+std::vector<Token> tokenize(const std::string& input) {
+    std::vector<Token> tokens;
+    for (size_t i = 0; i < input.length(); i++) {
+        if (isspace(input[i])) {
+            continue;
+        }
+        TokenType type = categorizeCharForMath(input[i]);
 
-int execute() {
+        if (type == NUM) { // when we have numbers, we need to parse the whole number
+            char* end;
+            double value = std::strtod(&input[i], &end);
+            tokens.push_back({NUM, value});
+            i = end - &input[0] - 1;
+        } else {
+            tokens.push_back({type, 0});
+        }
+    }
+    return tokens;
+}
+   
+int main() {
     std::string input;
     std::cout << "Enter an expression: ";
     std::cin >> input;
-    std::string values = parseInput(input);
 
-    printf("input: %s\n", input);  
-    printf("value: %d\n", values.c_str());
-    return 0;
-}
+    std::vector<Token> tokens = tokenize(input);
 
-int main() {
-    execute();
+    std::cout << "Tokens: ";
+
+    for (const auto& token : tokens) {
+        switch (token.type) {
+            case ADD: std::cout << "+"; break;
+            case SUB: std::cout << "-"; break;
+            case MUL: std::cout << "*"; break;
+            case DIV: std::cout << "/"; break;
+            case L_PAREN: std::cout << "("; break;
+            case R_PAREN: std::cout << ")"; break;
+            case NUM: std::cout << token.value; break;
+            case END: std::cout << "END"; break;
+        }
+        std::cout << ",";
+    }
     return 0;
-}
+} 
