b49b68ad07e32e2a45bcd76b2723b04a534e1737
a-carney <77558389+a-carney@users.noreply.github.com>
2023-11-28 17:47:51 -0500

    change from stack to trees
    changes to use of trees with grammatical rules in readME file

--- driver.cpp
+++ driver.cpp
@@ -1,28 +1,21 @@
 #include <iostream>
 #include <string>
-#include <vector>
+#include <memory>
+#include <stdexcept>
 #include <cctype>
 #include <cstdlib>
-#include <stack>
 
 
 
 enum TokenType {
-    ADD = 0, 
-    SUB = 1, 
-    MUL = 2, 
-    DIV = 3, 
-    L_PAREN = 4,
-    R_PAREN = 5,
-    NUM = 6, 
-    END = 7 
-};
-
-enum PEMDAS {
-    PARENTHESES = 3,
-    EXPONENT = 2,
-    MULTIPLY_DIVIDE = 1,
-    ADD_SUBTRACT = 0,
+    ADD, 
+    SUB, 
+    MUL, 
+    DIV, 
+    L_PAREN,
+    R_PAREN,
+    NUM, 
+    END 
 };
 
 struct Token {
@@ -30,182 +23,147 @@ struct Token {
     double value;
 };
 
+struct TreeNode {
+    Token token;
+    std::shared_ptr<TreeNode> left;
+    std::shared_ptr<TreeNode> right;
 
+    TreeNode(Token token) : token(token), left(nullptr), right(nullptr) {}
+};
 
-/**
- * @brief check if a char is a mathematic symbol
- * @returns 
- */
-TokenType categorizeCharForMath(char c) {
-    switch (c) {
-        case '+': return ADD;
-        case '-': return SUB;
-        case '*': return MUL;
-        case '/': return DIV;
-        case '(': return L_PAREN;
-        case ')': return R_PAREN;
-        default: return isdigit(c) ? NUM : END;
-    } 
-}
+class Parser {
+private:
+    std::string input;
+    size_t index = 0;
+    Token currentToken;
 
-/**
- * @brief returns order of operations for a given symbol 
- * @returns order of operations 
-*/
-int getOrderOfOperations(TokenType type) {
-    switch (type) {
-        case ADD: case SUB: return ADD_SUBTRACT;
-        case MUL: case DIV: return MULTIPLY_DIVIDE;
-        case L_PAREN: case R_PAREN: return PARENTHESES;
-        default: return -1;
-    }
-}
-
-/**
- * @brief creates tokenized vector of input
-*/
-std::vector<Token> tokenize(const std::string& input) {
-    std::vector<Token> tokens;
-    for (size_t i = 0; i < input.length(); i++) {
-        if (isspace(input[i])) {
-            continue;
+    void nextToken() {
+        while (index < input.length() && isspace(input[index])) {
+            index++;
         }
-        TokenType type = categorizeCharForMath(input[i]);
+        if (index == input.length()) {
+            currentToken = {END, 0};
+            return;
+        }
 
-        if (type == NUM) { // when we have numbers, we need to parse the whole number
-            char* end;
-            double value = std::strtod(&input[i], &end);
-            tokens.push_back({NUM, value});
-            i = end - &input[0] - 1;
+        char curr = input[index];
+        if (isdigit(curr)) {
+            std::string number;
+            while (index < input.length() && isdigit(input[index])) {
+                number += input[index++];
+            }
+            currentToken = {NUM, std::stod(number)};
         } else {
-            tokens.push_back({type, 0});
-        }
-    }
-    return tokens;
-}
-
-/**
- * @brief converts vector of tokens to postfix notation
-*/
-std::vector<Token> toPostfix(const std::vector<Token>& tokens) {
-    std::vector<Token> postfix;
-    std::stack<Token> stack;
-
-    for (const auto& token : tokens) {
-        switch (token.type) {
-            case NUM: 
-                postfix.push_back(token); // if it's a number, push it to the postfix vector
-                break;
-            case ADD: 
-            case SUB: 
-            case MUL: 
-            case DIV: { // if it's an operator, pop all operators with higher precedence off the stack and push them to the postfix vector
-                while (!stack.empty() && getOrderOfOperations(stack.top().type) >= getOrderOfOperations(token.type)) { 
-                    postfix.push_back(stack.top());
-                    stack.pop();
+            switch (curr) {
+                case '+': currentToken = {ADD, 0}; break;
+                case '-': currentToken = {SUB, 0}; break;
+                case '*': currentToken = {MUL, 0}; break;
+                case '/': currentToken = {DIV, 0}; break;
+                case '(': currentToken = {L_PAREN, 0}; break;
+                case ')': currentToken = {R_PAREN, 0}; break;
+                default: throw std::runtime_error("Unexpected Error");
                 }
-                stack.push(token);
-                break;
-            }
-            case L_PAREN: // if it's a left parenthesis, push it to the stack 
-                stack.push(token); 
-                break;
-            case R_PAREN: { // if it's a right parenthesis, pop all operators off the stack until we find a left parenthesis
-                while (!stack.empty() && stack.top().type != L_PAREN) {
-                    postfix.push_back(stack.top());
-                    stack.pop();
+            index++;
+            }        
+    }
+
+    std::shared_ptr<TreeNode> expr();
+    std::shared_ptr<TreeNode> term();
+    std::shared_ptr<TreeNode> factor();
+
+public:
+    Parser(const std::string& input) : input(input), index(0) {
+        nextToken();
+    }
+    std::shared_ptr<TreeNode> parse() {
+        return expr();
+    }
+};
+
+std::shared_ptr<TreeNode> Parser::expr() {
+    std::shared_ptr<TreeNode> node = term();
+    while (currentToken.type == ADD || currentToken.type == SUB) {
+        Token token = currentToken;
+        nextToken();
+        node = std::make_shared<TreeNode>(token);
+        node->left = node;
+        node->right = term();
+    }
+    return node;
+}
+
+std::shared_ptr<TreeNode> Parser::term() {
+    std::shared_ptr<TreeNode> node = factor();
+    while (currentToken.type == MUL || currentToken.type == DIV) {
+        Token token = currentToken;
+        nextToken();
+        node = std::make_shared<TreeNode>(token);
+        node->left = node;
+        node->right = factor();
+    }
+    return node;
+}
+
+std::shared_ptr<TreeNode> Parser::factor() {
+    Token token = currentToken;
+    if (token.type == NUM) {
+        nextToken();
+        return std::make_shared<TreeNode>(token);
+    } else if (token.type == L_PAREN) {
+        nextToken();
+        std::shared_ptr<TreeNode> node = expr();
+        if (currentToken.type != R_PAREN) {
+            throw std::runtime_error("Expected ')'");
+        }
+        nextToken();
+        return node;
+    } else {
+        throw std::runtime_error("Expected number or '('");
+    }
+}
+
+double evaluateTree(const std::shared_ptr<TreeNode>& node) {
+    if (!node) {
+        return 0;
+    }
+
+    if (node->token.type == NUM) {
+        return node->token.value;
+    } else {
+
+        double left = 0;
+        if (node->left) {
+            left = evaluateTree(node->left);
+        }
+        double right = 0;
+        if (node->right) {
+            right = evaluateTree(node->right);
+        }
+        switch (node->token.type) {
+            case ADD: return left + right;
+            case SUB: return left - right;
+            case MUL: return left * right;
+            case DIV: 
+                if (right == 0) { 
+                    throw std::runtime_error("Division by zero");
                 }
-                if (!stack.empty()) {
-                    stack.pop();
-                }
-                break;
-            }
-            default: // if it's an unexpected token, do nothing
-                break;
-            }
-        }
-        while (!stack.empty()) { // if there are still operators on the stack, pop them off and push them to the postfix vector
-            postfix.push_back(stack.top());
-            stack.pop();
-    }  
-    return postfix;
-}
-
-/**
- * @brief evaluates postfix expression
-*/
-double evaluatePostfix(const std::vector<Token>& postfix) {
-    std::stack<double> stack;
-
-    for (const auto& token : postfix) {
-        if (token.type == NUM) { // if it's a number, push it to the stack
-            stack.push(token.value);
-        } else { // if it's an operator, pop two numbers off the stack, apply the operator, and push the result back to the stack
-            double right = stack.top();
-            stack.pop();
-            double left = stack.top();
-            stack.pop();
-
-            switch (token.type) { // performs operations and pushes result to stack
-                case ADD: stack.push(left + right); break;
-                case SUB: stack.push(left - right); break;
-                case MUL: stack.push(left * right); break;
-                case DIV: stack.push(left / right); break;
-                default: break; // if unexpected operator, do nothing
-            }
+                return left / right;
+            default: throw std::runtime_error("Unexpected Error");
         }
     }
-    return stack.top();
 }
-
-/**
- * @brief FOR DEBUG -> turn this on to see the tokens and postfix notation
-*/
-void debugInfo(const std::vector<Token>& tokens, const std::vector<Token>& postfix) {
-    printf("Tokens: ");
-    for (const auto& token : tokens) {
-        switch (token.type) {
-        case ADD: printf("+ "); break;
-        case SUB: printf("- "); break;
-        case MUL: printf("* "); break;
-        case DIV: printf("/ "); break;
-        case L_PAREN: printf("( "); break;
-        case R_PAREN: printf(") "); break;
-        case NUM: printf("%f ", token.value); break;
-        case END: printf("END "); break;
-        }
-    }
-    printf("\nPostfix: ");
-    for (const auto& token : postfix) {
-        switch (token.type) {
-        case ADD: printf("+ "); break;
-        case SUB: printf("- "); break;
-        case MUL: printf("* "); break;
-        case DIV: printf("/ "); break;
-        case NUM: printf("%f ", token.value); break;
-        }
-    }
-    printf("\n");
-}
-    
    
 int main() {
+    std::string input;
+    std::cout << "Enter an expression: ";
+    std::getline(std::cin, input);
 
-    bool running = true;
-
-    while (running) {
-        std::string input;
-        std::cout << "Enter an expression: ";
-        std::cin >> input;
-        std::vector<Token> tokens = tokenize(input);
-        std::vector<Token> postfix = toPostfix(tokens);
-        debugInfo(tokens, postfix);
-        double result = evaluatePostfix(postfix);
-        printf("Result: %f\n", result);
-
-        std::cout << "Continue? (y/n): ";
-        std::cin >> input;
-        running = input == "y";
+    try {
+        Parser parser(input);
+        std::shared_ptr<TreeNode> root = parser.parse();
+        std::cout << evaluateTree(root) << std::endl;
+    } catch (const std::exception& e) {
+        std::cout << e.what() << std::endl;
     }
     return 0;
 }
